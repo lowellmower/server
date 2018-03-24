d83a56fe90964614ef10fadb9d0e2b177727d99b
Lowell Mower <lowell.m.mower@gmail.com>
2018-03-24 12:25:41 -0400

    Add index.html - ship it :D

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,22 @@ fn main() {
 }
 
 fn hand_conn(mut stream: TcpStream) {
+    // assign variables
     let resp_200 = "HTTP/1.1 200 OK\r\n\r\n";
     let mut buffer = [0; 512];
 
+    // unwrap stream to the alloted buffer size
     stream.read(&mut buffer).unwrap();
+
+    // visual validation / debugging
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
+    let mut index_file = File::open("./index.html").unwrap();
+    let mut index_resp = String::new();
+    index_file.read_to_string(&mut index_resp).unwrap();
+
+    // write the response to socket
     stream.write(resp_200.as_bytes()).unwrap();
-    stream.flush();
+    stream.write(index_resp.as_bytes()).unwrap();
+    let _ignore = stream.flush();
 }
